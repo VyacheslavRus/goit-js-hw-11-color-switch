fdd94e95d055a3ca5cb4697746fcb35a87869283
VyacheslavRus <78310890+VyacheslavRus@users.noreply.github.com>
2021-04-07 23:11:52 +0300

    add ready switch-color

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
+const colors = [
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
+  ];
 
 
+  
+  const startBtn = document.querySelector('[data-action="start"]');
+  const stopBtn = document.querySelector('[data-action="stop"]');  
+  
+  let intervalId = null
 
+  const start = () =>{
+      const randomIntegerFromInterval = (min, max) => {
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    };
+      intervalId = setInterval(() => {
+          document.body.style.background = colors[randomIntegerFromInterval(0, colors.length-1)];
+      }, 1000);
+      startBtn.removeEventListener('click', start)
+  }
 
-
-
-
-
-
-
-
-
-
+   const stop = () => {
+       clearInterval(intervalId)
+       startBtn.addEventListener('click', start);
+    }
+  
+    startBtn.addEventListener('click', start);
+    stopBtn.addEventListener('click', stop);
